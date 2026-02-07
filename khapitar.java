39e7825a2cde0128924af9f7e84229138274bbea
Adarsh Chaudhary <chaudharyadarsh97601@gmail.com>
2026-02-07 15:59:39 +0530

    new text khapitar

--- khapitar.java
+++ khapitar.java
@@ -1,5 +1,5 @@
-public class tanishka {
+public class khapitar {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, khapitar!");
     }
 }
